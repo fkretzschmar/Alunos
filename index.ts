176642e7c979418f4ffefe6544d08670f2314d6d
felipek <ffkretzschmar@gmail.com>
2023-10-25 22:28:37 -0300

    alterado alguma coisa

--- index.ts
+++ index.ts
@@ -9,8 +9,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
+const options = {
+  swaggerOptions: {
+    url: 'https://gist.githubusercontent.com/fkretzschmar/0ac05c13c3dac3edd69d4945f9f0b727/raw/6833f704f09ba1bea4eb9bcd22520401e7d5738d/swagger.json'
+  }
+}
+
 app.use("/", router);
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(undefined, options));
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
