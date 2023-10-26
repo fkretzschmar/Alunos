7a4736cbc58b0d868156cb97a388dbef0424cda1
felipek <ffkretzschmar@gmail.com>
2023-10-25 21:46:58 -0300

    inclui pg

--- db/conection.ts
+++ db/conection.ts
@@ -1,12 +1,14 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import pg from "pg"
 
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.POSTGRES_DATABASE || "",
     process.env.POSTGRES_USER || "", process.env.POSTGRES_PASSWORD, {
     host: process.env.POSTGRES_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectModule: pg
 });
 console.log(process.env.POSTGRES_PASSWORD)
 export default sequelize;
